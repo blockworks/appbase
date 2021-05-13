515645b0d786b12eb3672fb9e24cb64279a95d1f
blockworks <blockworks2@gmail.com>
2021-05-13 14:49:40 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const App1 = {
+  data(){
+    return{
+      xxxxx: '',
+      xxxxx:[
+        "",
+        ""
+      ]
+    }
+  },
+  methods: {
+    functionName: function() {
+			//todo
+    },
+    functionName: function() {
+			//todo
+    }
+  },
+	computed:{
+		functionName: function() {
+			//todo
+    },
+		functionName: function() {
+			//todo
+    }
+}//App1
+app1 = Vue.createApp(App1);
+app1.mount('#App');
